54de46c3ec5ae6a9d20cbe8044bed2a17ce74f4b
acema <ace.marino@gmail.com>
2021-07-13 13:59:00 -0400

    checking data pul;l
    test pulled all songs from an artist

--- webscraper.py
+++ webscraper.py
@@ -10,7 +10,7 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://music.youtube.com/browse/VLPL6Ri1UOk51t5KtpuB5KNL9PVdSywUs_d8")
 driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-time.sleep(3)
+time.sleep(5)
 elem = driver.find_elements_by_xpath('//yt-formatted-string[@class="title style-scope ytmusic-responsive-list-item-renderer complex-string"]')
 songs=[]
 for s in range(len(elem)):
