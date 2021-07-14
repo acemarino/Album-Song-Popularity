ef48efa9db7d17564d79e7ede3fd8b07b41215d8
acema <ace.marino@gmail.com>
2021-07-13 22:11:34 -0400

    implemented site automation
    added in code to have script search youtube music for the selected  artist

--- webscraper.py
+++ webscraper.py
@@ -8,7 +8,20 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
-driver.get("https://music.youtube.com/browse/VLPL6Ri1UOk51t5KtpuB5KNL9PVdSywUs_d8")
+driver.get("https://music.youtube.com/")
+searchButton=driver.find_element_by_class_name("style-scope tp-yt-paper-icon-button")
+searchButton.click()
+textArea=driver.find_element_by_xpath("//input[@placeholder='Search']")
+artist='BTS'
+textArea.send_keys(artist)
+textArea.send_keys(Keys.RETURN)
+time.sleep(3)
+Artist=driver.find_element_by_xpath("//div[@id='contents']/ytmusic-responsive-list-item-renderer/a")
+Artist.click()
+time.sleep(3)
+showAll=driver.find_element_by_xpath("//div[@id='contents']/ytmusic-shelf-renderer/div[5]/a/tp-yt-paper-button/yt-formatted-string")
+showAll.click()
+time.sleep(3)
 driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 time.sleep(5)
 elem = driver.find_elements_by_xpath('//yt-formatted-string[@class="title style-scope ytmusic-responsive-list-item-renderer complex-string"]')
