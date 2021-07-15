1a73e47171217ae9854b480138ba8f2aab494b23
acema <ace.marino@gmail.com>
2021-07-14 20:34:23 -0400

    passing data to json
    workring on getting python objects to pass to a json

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,6 @@
 from functools import total_ordering
-import requests
 import re
 import json
-import html
-import html5lib
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -18,7 +15,7 @@ searchButton.click()
 
 #enters desired search term(s)
 textArea=driver.find_element_by_xpath("//input[@placeholder='Search']")
-artist='BTS'
+artist='Wilbur'
 textArea.send_keys(artist)
 textArea.send_keys(Keys.RETURN)
 time.sleep(3)
@@ -42,14 +39,14 @@ elem = driver.find_elements_by_xpath('//yt-formatted-string[@class="title style-
 songs=[]
 for s in range(len(elem)):
     songs.append(elem[s].text)
-time.sleep(3)
+time.sleep(2)
 
 #album title
 elem = driver.find_elements_by_xpath("//div[@id='contents']/ytmusic-responsive-list-item-renderer/div[2]/div[3]/yt-formatted-string[2]/a")
 albums=[]
 for s in range(len(elem)):
     albums.append(elem[s].text)
-time.sleep(3)
+time.sleep(2)
 
 #song length
 elem = driver.find_elements_by_xpath('//yt-formatted-string[@size="MUSIC_RESPONSIVE_LIST_ITEM_FIXED_COLUMN_SIZE_SMALL"]')
@@ -58,22 +55,21 @@ for s in range(len(elem)):
     times.append(elem[s].text)
 driver.close()
 
-class Total:
+#creating objects that contain song name, album, and song time 
+class SongObj:
     
     def __init__(self,song,album,times):
         self.song=song
         self.album=album
         self.times=times
-totalObjects=[]
+
+songObjs=[]
 for i in range(len(elem)):
-    newThing = Total(songs[i],albums[i],times[i])
-    totalObjects.append(newThing)
+    newThing = SongObj(songs[i],albums[i],times[i])
+    songObjs.append(newThing)
 
-print(totalObjects[3].song)
-
-
-
+print(songObjs[0].song,songObjs[0].album,songObjs[0].times)
 #pushing pulled data to a json file
-with open('songData.json', 'w') as outfile:
-    json.dump(songs, outfile)
+#with open('songData.json', 'w') as outfile:
+    #json.dump(totalObjects, outfile)
     
